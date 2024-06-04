7a44e2d9a08e12bba7caed938fdbbdfcbbe1de7a
Matys Laptop <bartosz.matysek1@gmail.com>
2024-06-04 10:28:45 +0200

    Add database

--- caloriesdb.sql
+++ caloriesdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Cze 04, 2024 at 04:54 AM
+-- Generation Time: Cze 04, 2024 at 10:26 AM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -42,8 +42,10 @@ CREATE TABLE `dish` (
 --
 
 INSERT INTO `dish` (`idDish`, `dishName`, `totalCalories`, `totalCarbohydrates`, `totalProteins`, `totalFats`, `idUser`) VALUES
-(2, 'Danie', 0, 0, 0, 0, 2),
-(4, 'asdasd', 0, 0, 0, 0, 2);
+(2, 'Danie', 146.68, 9.04, 20.07, 7.56, 2),
+(4, 'asdasd', 0, 0, 0, 0, 2),
+(12, 'Zupa', 6.33, 1.395, 0.165, 0.0225, 3),
+(13, 'Zupa123', 0, 0, 0, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -56,6 +58,16 @@ CREATE TABLE `dishproducts` (
   `idProduct` int(11) NOT NULL,
   `quantity` float NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `dishproducts`
+--
+
+INSERT INTO `dishproducts` (`idDish`, `idProduct`, `quantity`) VALUES
+(12, 27, 15),
+(2, 21, 10),
+(2, 9, 50),
+(2, 26, 100);
 
 -- --------------------------------------------------------
 
@@ -91,7 +103,7 @@ INSERT INTO `product` (`idProduct`, `productName`, `calories`, `carbohydrates`, 
 (24, 'Kiwi', 65.2, 14.7, 1.1, 0.5),
 (25, 'Koperek', 46.4, 7, 3.5, 1.1),
 (26, 'Mięso mielone drobiowe (kurczak, indyk)', 106, 0, 19, 7.5),
-(27, 'Cebula', 42, 9.3, 1.1, 0.1),
+(27, 'Cebula', 42.2, 9.3, 1.1, 0.15),
 (28, 'Marchew (zasmażana)', 67.2, 11.8, 1, 4),
 (29, 'Jajko kurze', 91.2, 0.6, 12.5, 9.7),
 (30, 'Ptasie mleczko', 322.4, 56.2, 2.6, 21.8),
@@ -100,7 +112,7 @@ INSERT INTO `product` (`idProduct`, `productName`, `calories`, `carbohydrates`, 
 (33, 'Oscypek', 236, 0, 29, 30),
 (34, 'Kalafior', 30.4, 5, 2.4, 0.2),
 (35, 'Ser Feta', 158.4, 4.1, 14.2, 21.3),
-(46, 'asd', 94.22, 15.555, 4, 4);
+(48, 'kkk', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -147,21 +159,24 @@ CREATE TABLE `user` (
   `userName` varchar(45) NOT NULL,
   `creationDate` datetime NOT NULL,
   `editDate` datetime DEFAULT NULL,
-  `editedBy` int(11) DEFAULT NULL
+  `editedBy` int(11) DEFAULT NULL,
+  `deactivateDate` datetime DEFAULT NULL,
+  `isActive` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`idUser`, `login`, `password`, `userName`, `creationDate`, `editDate`, `editedBy`) VALUES
-(1, 'admin', '$2y$10$sEaYY2rpngzBRciJpnksueN.VzyIZb1r4Lev9N7wkXZL9rBPVc1pi', 'Administrator', '2024-05-17 23:26:22', NULL, NULL),
-(2, 'user', '$2y$10$9YkihbpZ7OBUAPZsUBQoNOd/V7hP.TWYmkx1i.G0riyqGEja4s3IS', 'Użytkownik1', '2024-05-17 23:38:16', '2024-06-03 19:51:21', 1),
-(3, 'dietician', '$2y$10$jTtROJ84NZ4DoiUrhq2P1eNZv0cB/oYebesgcx7Tf9wPJOFJ0lJ4q', 'Dietetyk', '2024-05-17 23:38:45', '2024-06-04 02:24:35', 1),
-(14, 'anna.nowak', '$2y$10$wcaroLhVPJJJjU4fnmn4WuOMyMFu3xZUIWlqi6W14epVk3e3QgYS.', 'Anna Nowak', '2024-06-03 19:43:54', '2024-06-03 19:45:28', 1),
-(15, 'maria.wojcik', '$2y$10$lJe9My/4XAj2gNNTh/QgY.PccRTuRheEAzJj9Nf2/3WaXRD1PDlKm', 'Maria Wojcik', '2024-06-03 19:44:19', NULL, NULL),
-(16, 'piotr.wisniewski', '$2y$10$N.sB0jfz.wWCSHefBQpcKu7RAbuYi90xXy9u1nUdioV1f/a/DWK1a', 'Pioter', '2024-06-03 19:44:48', '2024-06-03 21:15:57', 17),
-(17, 'Konrad', '$2y$10$fBFa2HrlMUJwhKnjKyaAqOUl4xZolFjrZ.RkIkAG4xE6QTSPz./my', 'Konrad', '2024-06-03 19:48:44', '2024-06-03 20:03:23', 1);
+INSERT INTO `user` (`idUser`, `login`, `password`, `userName`, `creationDate`, `editDate`, `editedBy`, `deactivateDate`, `isActive`) VALUES
+(1, 'admin', '$2y$10$sEaYY2rpngzBRciJpnksueN.VzyIZb1r4Lev9N7wkXZL9rBPVc1pi', 'Administrator', '2024-05-17 23:26:22', NULL, NULL, NULL, 1),
+(2, 'user', '$2y$10$9YkihbpZ7OBUAPZsUBQoNOd/V7hP.TWYmkx1i.G0riyqGEja4s3IS', 'Użytkownik1', '2024-05-17 23:38:16', '2024-06-04 07:01:26', 1, NULL, 1),
+(3, 'dietician', '$2y$10$jTtROJ84NZ4DoiUrhq2P1eNZv0cB/oYebesgcx7Tf9wPJOFJ0lJ4q', 'Dietetyk', '2024-05-17 23:38:45', '2024-06-04 07:05:50', 1, NULL, 1),
+(14, 'anna.nowak', '$2y$10$CpFYO2p97wLE6C4pl4wBt.r19W.LH7h0WDBGYFWXcVcI/qiTfwjVy', 'Anna Nowak', '2024-06-03 19:43:54', '2024-06-04 07:12:13', 17, '2024-06-04 07:12:13', 0),
+(15, 'maria.wojcik', '$2y$10$lJe9My/4XAj2gNNTh/QgY.PccRTuRheEAzJj9Nf2/3WaXRD1PDlKm', 'Maria Wojcik', '2024-06-03 19:44:19', NULL, NULL, NULL, 1),
+(16, 'piotr.wisniewski', '$2y$10$N.sB0jfz.wWCSHefBQpcKu7RAbuYi90xXy9u1nUdioV1f/a/DWK1a', 'Pioter', '2024-06-03 19:44:48', '2024-06-03 21:15:57', 17, NULL, 1),
+(17, 'Konrad', '$2y$10$fBFa2HrlMUJwhKnjKyaAqOUl4xZolFjrZ.RkIkAG4xE6QTSPz./my', 'Konrad', '2024-06-03 19:48:44', '2024-06-03 20:03:23', 1, NULL, 1),
+(22, 'taachii', '$2y$10$Gy9fGUHjaqdmReCW/RfSvun6kUJbyYLLs9qHhwsTpxlsPcYMNIp2y', 'taachii', '2024-06-04 08:55:57', '2024-06-04 09:00:42', NULL, '2024-06-04 09:00:42', 0);
 
 --
 -- Wyzwalacze `user`
@@ -173,6 +188,16 @@ DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `before_user_update` BEFORE UPDATE ON `user` FOR EACH ROW BEGIN
     SET NEW.editDate = NOW();
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_deactivate_date` BEFORE UPDATE ON `user` FOR EACH ROW BEGIN
+    -- Sprawdza, czy kolumna isActive została zmieniona z 1 na 0
+    IF OLD.isActive = 1 AND NEW.isActive = 0 THEN
+        -- Ustawia aktualną datę w kolumnie deactivateDate
+        SET NEW.deactivateDate = NOW();
+    END IF;
 END
 $$
 DELIMITER ;
@@ -206,9 +231,11 @@ INSERT INTO `userroles` (`idUser`, `idRole`, `assignDate`, `removeDate`) VALUES
 (17, 2, '2024-06-03 19:49:58', '2024-06-03 19:51:34'),
 (3, 3, '2024-06-03 19:50:37', '2024-06-03 19:50:42'),
 (3, 2, '2024-06-03 19:50:42', NULL),
-(2, 2, '2024-06-03 19:51:21', NULL),
+(2, 2, '2024-06-03 19:51:21', '2024-06-04 07:01:26'),
 (17, 3, '2024-06-03 19:51:34', '2024-06-03 20:03:23'),
-(17, 1, '2024-06-03 20:03:23', NULL);
+(17, 1, '2024-06-03 20:03:23', NULL),
+(2, 3, '2024-06-04 07:01:26', NULL),
+(22, 3, '2024-06-04 08:55:57', NULL);
 
 --
 -- Wyzwalacze `userroles`
@@ -272,13 +299,13 @@ ALTER TABLE `userroles`
 -- AUTO_INCREMENT for table `dish`
 --
 ALTER TABLE `dish`
-  MODIFY `idDish` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idDish` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `role`
@@ -290,7 +317,7 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
